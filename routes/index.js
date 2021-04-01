ae5ffe17138e21d6d9ccdc2ba6cd6d1af52db590
Rrezearta <rrezearta.thaqi@student.uni-pr.edu>
2021-04-01 21:23:51 +0200

    Krijimi i login per studentet dhe profesoret

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,15 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('login', { title: 'Login' });
+  res.render('index', { title: 'E-Shkolla' });
 });
+
+router.get('/loginPr', function(req, res, next) {
+  res.render('loginPr', { title: 'Login' });
+});
+router.get('/loginSt', function(req, res, next) {
+  res.render('loginSt', { title: 'Login' });
+});
+
 
 module.exports = router;
