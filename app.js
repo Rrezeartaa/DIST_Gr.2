db041486e2e67854a84f66686f66445b8919024f
Rrezearta <rrezearta.thaqi@student.uni-pr.edu>
2021-04-12 20:42:36 +0200

    Ndryshime ne Chat. Gati ne perfundim...

--- app.js
+++ app.js
@@ -1,19 +1,20 @@
-'use strict';
-
-var os = require('os');
-var http = require('http');
-var socketIO = require('socket.io');
 
 var createError = require('http-errors');
 var express = require('express');
+var app = express();
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+const server = require("http").Server(app);
+const { v4: uuidv4 } = require("uuid");
+app.set("view engine", "ejs");
+const io = require("socket.io")(server);
+const { ExpressPeerServer } = require("peer");
+const peerServer = ExpressPeerServer(server, {
+  debug: true,
+});
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-
-var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -25,76 +26,37 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use("/peerjs", peerServer);
+
 app.get("/chat", function(req, res){
-	res.render("students/student-chat");
+	res.redirect(`/${uuidv4()}`);  //students/student-chat
 });
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+app.get("/:room", function(req, res){
+	res.render("students/student-chat", { roomId: req.param.room });
+  });
+  
+  io.on("connection", function(socket){
+	socket.on("join-room", function(roomId, userId, userName){
+	  socket.join(roomId);
+	  socket.to(roomId).emit("user-connected", userId);
+	  socket.on("message", function(message) {
+		io.to(roomId).emit("createMessage", message, userName);
+	  });
+	});
+  });
+  
+  server.listen(process.env.PORT || 3001);  
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
-const server = http.createServer(app)
-server.listen(process.env.PORT || 3000);
-const io = socketIO(server);
 
-io.sockets.on('connection', function(socket) {
-
-	function log() {
-	  var array = ['Message from server:'];
-	  array.push.apply(array, arguments);
-	  socket.emit('log', array);
-	}
-  
-    //Defining Socket Connections
-    socket.on('message', function(message, room) {
-	  log('Client said: ', message);
-	  // for a real app, would be room-only (not broadcast)
-	  socket.in(room).emit('message', message, room);
-	});
-  
-	socket.on('create or join', function(room) {
-	  log('Received request to create or join room ' + room);
-  
-	  var clientsInRoom = io.sockets.adapter.rooms[room];
-	  var numClients = clientsInRoom ? Object.keys(clientsInRoom.sockets).length : 0;
-	  log('Room ' + room + ' now has ' + numClients + ' client(s)');
-  
-	  if (numClients === 0) {
-		socket.join(room);
-		log('Client ID ' + socket.id + ' created room ' + room);
-		socket.emit('created', room, socket.id);
-  
-	  } else if (numClients === 1) {
-		log('Client ID ' + socket.id + ' joined room ' + room);
-		io.sockets.in(room).emit('join', room);
-		socket.join(room);
-		socket.emit('joined', room, socket.id);
-		io.sockets.in(room).emit('ready');
-	  } else { // max two clients
-		socket.emit('full', room);
-	  }
-	});
-  
-	socket.on('ipaddr', function() {
-	  var ifaces = os.networkInterfaces();
-	  for (var dev in ifaces) {
-		ifaces[dev].forEach(function(details) {
-		  if (details.family === 'IPv4' && details.address !== '127.0.0.1') {
-			socket.emit('ipaddr', details.address);
-		  }
-		});
-	  }
-	});
-  
-	socket.on('bye', function(){
-	  console.log('received bye');
-	});
-  
-  });
 
 app.use(function(err, req, res, next) { 
 
