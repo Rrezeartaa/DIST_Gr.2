46b3a3cc7e44abe2af2b367ef5e1aca80bc85174
Rrezearta <rrezearta.thaqi@student.uni-pr.edu>
2021-05-10 19:28:18 +0200

    Update packages

--- restclient/src/main/java/com/projekti/App.java
+++ restclient/src/main/java/com/projekti/App.java
@@ -1,6 +1,7 @@
 package com.projekti;
 import com.squareup.okhttp.MediaType;
 import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
 import com.squareup.okhttp.RequestBody;
 import com.squareup.okhttp.Response;
 import java.util.Date;
