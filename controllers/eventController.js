6a25beb4963728c0ac7c2736c4311b7b842b9d43
Rrezearta <rrezearta.thaqi@student.uni-pr.edu>
2021-05-10 20:31:47 +0200

    Event changes

--- controllers/eventController.js
+++ controllers/eventController.js
@@ -29,4 +29,45 @@ class EventController {
     }
   }
 
-  
+  createEvent (req, res, next) {
+    try {
+      const { text } = req.body
+      const id = this.list.length > 0 ? this.list[this.list.length - 1].id + 1 : 1
+      const event = new Event({ id, title, event_date, theme })
+      this.list.push(event)
+
+      return res.status(HttpStatus.CREATED).json(id)
+    } catch (err) {
+      next(err)
+    }
+  }
+
+  updateEvent (req, res, next) {
+    try {
+      const id = parseInt(req.params.id, 10)
+      const { title, event_date, theme } = req.body
+      const event = this.list.find(x => x.id === id)
+      if (!event) throw new createError.NotFound()
+      event.title = title
+      event.event_date = event_date
+      event.theme = theme
+
+      return res.status(HttpStatus.OK).end()
+    } catch (err) {
+      next(err)
+    }
+  }
+
+  deleteEvent (req, res, next) {
+    try {
+      const id = parseInt(req.params.id, 10)
+      const event = this.list.find(x => x.id === id)
+      if (!event) throw new createError.NotFound()
+      this.list = this.list.filter(x => x.id !== id)
+
+      return res.status(HttpStatus.OK).end()
+    } catch (err) {
+      next(err)
+    }
+  }
+}
