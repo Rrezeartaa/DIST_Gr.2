126c98ee70dc074fab4320ecb78e7662ede61e50
Rrezearta <rrezearta.thaqi@student.uni-pr.edu>
2021-06-02 22:31:23 +0200

    Updated chat

--- app.js
+++ app.js
@@ -3,24 +3,21 @@ var createError = require('http-errors');
 var express = require('express');
 var app = express();
 var path = require('path');
+let stream = require( 'C:/Users/Admin/DIST_PROJECT_Gr.2/ws/stream' );
+// let favicon = require( 'serve-favicon' );
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const server = require("http").Server(app);
-const { v4: uuidv4 } = require("uuid");
+// const { v4: uuidv4 } = require("uuid");
 app.set("view engine", "ejs");
-const io = require("socket.io")(server, {
-  cors: {
-    origin: '*'
-  }
-});
+const io = require("socket.io")(server);
 
-const { ExpressPeerServer } = require("peer");
-const peerServer = ExpressPeerServer(server, {
-  debug: true,
-});
+// const { ExpressPeerServer } = require("peer");
+// const peerServer = ExpressPeerServer(server, {
+//   debug: true,
+// });
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
@@ -31,58 +28,57 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+// app.use( favicon( path.join( __dirname, 'favicon.ico' ) ) );
+// app.use("/peerjs", peerServer);
 
-app.use("/peerjs", peerServer);
-
-// app.get("/chat", function(req, res){
-// 	res.redirect(`/${uuidv4()}`);  //students/student-chat
-// });
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-app.get("/:room", function(req, res){
-	res.render("students/student-chat", { roomId: req.param.room });
-  });
-
-// app.get('/', (req, res) => {
-//     res.render('index.pug', {
-//         title: 'Home Page'
-//     });
+// app.get("/chat", (req, res) => {
+//   res.redirect(`/${uuidv4()}`);
 // });
 
-// app.get('/about', (req, res) => {
-//     res.render('Student.pug', {
-//         title: 'About Page'
-//     });
+// app.get("/:room", (req, res) => {
+//   res.render("students/student-chat", { roomId: req.params.room });
 // });
 
-  io.on("connection", (socket)=>{
-	socket.on("join-room", (roomId, userId, userName)=>{
-	  socket.join(roomId);
-	  socket.to(roomId).emit("user-connected", userId);
-	  socket.on("message", (message)=> {
-		io.to(roomId).emit("createMessage", message, userName);
-	  });
-	});
-  });
+// // io.listen(4000);
 
-  server.listen(process.env.PORT || 5000);  
+// io.on("connection", (socket)=>{
+// 	socket.on("join-room", (roomId, userId, userName)=>{
+// 	  socket.join(roomId);
+//     socket.to(roomId).broadcast.emit("user-connected", userId);
+// 	  socket.on("message", (message)=>{
+// 		io.to(roomId).emit("createMessage", message, userName);
+// 	  });
+// 	});
+//   });
 
+io.of( '/stream' ).on( 'connection', stream );
+
+server.listen( 3000 );
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+// app.use(function(req, res, next) {
+//   next(createError(404));
+// });
 
 
-app.use(function(err, req, res, next) { 
+// app.use(function(err, req, res, next) { 
 
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+//   res.locals.message = err.message;
+//   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  res.status(err.status || 500);
-  res.render('error');
-});
+//   res.status(err.status || 500);
+//   res.render('error');
+// });
 
-module.exports = app;
+// module.exports = app;
+
+
+
+
+
+
+
