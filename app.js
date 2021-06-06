035c65fc18d79828b0dc30f05b8a388e36789b7a
Rrezearta <rrezearta.thaqi@student.uni-pr.edu>
2021-06-06 18:37:00 +0200

    added passport for authentication

--- app.js
+++ app.js
@@ -1,80 +1,91 @@
-
 var createError = require('http-errors');
+const session = require("express-session");
+const flash = require("express-flash");
 var express = require('express');
+var passport = require('passport');
 var app = express();
 var path = require('path');
 let stream = require( 'C:/Users/Admin/DIST_PROJECT_Gr.2/ws/stream' );
-// let favicon = require( 'serve-favicon' );
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const server = require("http").Server(app);
-// const { v4: uuidv4 } = require("uuid");
 app.set("view engine", "ejs");
 const io = require("socket.io")(server);
+require("dotenv").config();
+const initializePassport = require("C:/Users/Admin/DIST_PROJECT_Gr.2/controllers/passport.js");
 
-// const { ExpressPeerServer } = require("peer");
-// const peerServer = ExpressPeerServer(server, {
-//   debug: true,
-// });
+initializePassport(passport);
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-// view engine setup
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
-
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(cookieParser('12345'));
 app.use(express.static(path.join(__dirname, 'public')));
-// app.use( favicon( path.join( __dirname, 'favicon.ico' ) ) );
-// app.use("/peerjs", peerServer);
-
+app.use(
+    session({
+      secret: process.env.SESSION_SECRET,
+      resave: false,
+      saveUninitialized: false
+    })
+  );
+app.use(passport.initialize())
+app.use(passport.session())
+app.use(flash());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-// app.get("/chat", (req, res) => {
-//   res.redirect(`/${uuidv4()}`);
-// });
-
-// app.get("/:room", (req, res) => {
-//   res.render("students/student-chat", { roomId: req.params.room });
-// });
-
-// // io.listen(4000);
-
-// io.on("connection", (socket)=>{
-// 	socket.on("join-room", (roomId, userId, userName)=>{
-// 	  socket.join(roomId);
-//     socket.to(roomId).broadcast.emit("user-connected", userId);
-// 	  socket.on("message", (message)=>{
-// 		io.to(roomId).emit("createMessage", message, userName);
-// 	  });
-// 	});
-//   });
-
 io.of( '/stream' ).on( 'connection', stream );
+
+
+app.get("/loginSt", checkAuthenticated, (req, res) => {
+    // flash sets a messages variable. passport sets the error message
+    console.log(req.session.flash.error);
+    res.render("loginSt");
+  });
+  
+  app.get("/index", checkNotAuthenticated, (req, res) => {
+    console.log(req.isAuthenticated());
+    res.render("students/faqjakryesore", { user: req.user.name });
+  });
+  
+  app.get("/logout", (req, res) => {
+    req.logout();
+    res.render("loginSt", { message: "You have logged out successfully" });
+  });
+
+  
+app.post(
+    "/loginSt",
+    passport.authenticate("local", {
+      successRedirect: "/index",
+      failureRedirect: "/loginSt",
+      failureFlash: true
+    })
+  );
+  
+  function checkAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) {
+      return res.redirect("/index");
+    }
+    next();
+  }
+  
+  function checkNotAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) {
+      return next();
+    }
+    res.redirect("/loginSt");
+  }
 
 server.listen( 3000 );
 
-// catch 404 and forward to error handler
-// app.use(function(req, res, next) {
-//   next(createError(404));
-// });
-
-
-// app.use(function(err, req, res, next) { 
-
-//   res.locals.message = err.message;
-//   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-//   res.status(err.status || 500);
-//   res.render('error');
-// });
-
-// module.exports = app;
 
 
 
---
+++ controllers/passport.js
@@ -0,0 +1,63 @@
+const LocalStrategy = require("passport-local").Strategy;
+const { pool } = require("../databaza/konektimi");
+const bcrypt = require("bcrypt");
+
+function initialize(passport) {
+  console.log("Initialized");
+
+  const authenticateUser = (idS, password, done) => {
+    console.log(idS, password);
+    pool.query(
+      `SELECT * FROM students WHERE idS = $1`,
+      [idS],
+      (err, results) => {
+        if (err) {
+          throw err;
+        }
+        console.log(results.rows);
+
+        if (results.rows.length > 0) {
+          const user = results.rows[0];
+
+          bcrypt.compare(password, user.password, (err, isMatch) => {
+            if (err) {
+              console.log(err);
+            }
+            if (isMatch) {
+              return done(null, user);
+            } else {
+              //password eshte gabim
+              return done(null, false, { message: "Password is incorrect" });
+            }
+          });
+        } else {
+          // Nuk ka user
+          return done(null, false, {
+            message: "No user with that email address"
+          });
+        }
+      }
+    );
+  };
+
+  passport.use(
+    new LocalStrategy(
+      { usernameField: "idS", passwordField: "password" },
+      authenticateUser
+    )
+  );
+ 
+  passport.serializeUser((user, done) => done(null, user.id));
+
+  passport.deserializeUser((id, done) => {
+    pool.query(`SELECT * FROM students WHERE id = $1`, [id], (err, results) => {
+      if (err) {
+        return done(err);
+      }
+      console.log(`ID is ${results.rows[0].id}`);
+      return done(null, results.rows[0]);
+    });
+  });
+}
+
+module.exports = initialize;
