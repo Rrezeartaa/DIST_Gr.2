72e3ee171712db0468e38b9a8db4e64a5a4a1aee
Rrezearta <rrezearta.thaqi@student.uni-pr.edu>
2021-07-06 00:59:45 +0200

    Update

---
+++ java/src/main/java/com/example/java/controllers/LuController.java
@@ -0,0 +1,35 @@
+package com.example.java.controllers;
+import com.example.java.exception.ResourceNotFoundException;
+import com.example.java.models.Lendet;
+import com.example.java.models.Literatura;
+import com.example.java.models.Lu;
+import com.example.java.models.User;
+import com.example.java.repository.LendetRepository;
+import com.example.java.repository.LuRepository;
+import com.example.java.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+import javax.validation.Valid;
+
+public class LuController {
+
+    @Autowired
+    private LuRepository luRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+    
+    // @PostMapping("/notat/{lenda_id}/{user_id}")
+    // public Lu createNota(@PathVariable Long user_id, @Valid @RequestBody Lu lu) {
+        
+    //     return userRepository.findById(user_id)
+    //             .map(user -> {
+    //                 lu.setUser(user);
+    //                 return luRepository.save(lu);
+    //             }).orElseThrow(() -> new ResourceNotFoundException("User me id " + user_id + " nuk u gjet!"));
+    // }
+}
--- public/javascripts/lendetController.js
+++ public/javascripts/lendetController.js
@@ -5,30 +5,11 @@ function getItems() {
     .catch(error => console.error('Unable to get items.', error));
 }
 
-// function addLiterature(id) {
-//   const emri = document.getElementById('name');
-//   const description = document.getElementById('description');
-//   const author = document.getElementById('author');
-//   const file = document.getElementById('file');
-
-//   // upload(file);
-
-//   const item = {
-//     emri: emri.value,
-//     description: description.value,
-//     author: author.value,
-//     file: file.files[0].name
-//   };
-
-//   fetch(`http://localhost:8080/literature/${id}`,{
-
 function addLenda(id) {
   const emri = document.getElementById('name');
-  // const select = document.getElementById('select');
 
   const item = {
     emri: emri.value,
-    // user_id: 2
   };
 
   console.log(emri.value)
@@ -51,55 +32,7 @@ function deleteItem(id) {
   fetch(`${url}/${id}`, {
     method: 'DELETE'
   }) 
-  .then(() => getItems())
-  //e rregullon qe kur te bohet delete mu fshi paraprakja
+  // .then(() => location.reload())
   .catch(error => console.error('Unable to delete item.', error));
 }
 
-function _displayItems(data) {
-  // const tBody = document.getElementById('todos');
-  // const button = document.createElement('button');
-
-  data.forEach(item => {
-
-    // let editButton = button.cloneNode(false);
-    // editButton.innerText = 'Edit';
-    // editButton.setAttribute('onclick', `displayEditForm('${item.ngjarjaid}')`);
-    // editButton.style.marginRight = "20px";
-    // editButton.style.padding = "6px 10px 6px 10px";
-    // editButton.style.backgroundColor = "green";
-    // editButton.style.color = "white";
-    // editButton.style.borderRadius = "5px";
-
-    // let deleteButton = button.cloneNode(false);
-    // deleteButton.innerText = 'Delete';
-    // deleteButton.setAttribute('onclick', `deleteItem('${item.id}')`);
-    // deleteButton.style.color = "red";
-
-
-    document.getElementById('id').setContent = item.id;
-
-    document.getElementById('emri').setContent = item.emri;
-    // let tr = tBody.insertRow();
-
-    // let td2 = tr.insertCell(0);
-    // let textNode = document.createTextNode(item.title);
-    // td2.appendChild(textNode);
-
-    // let td3 = tr.insertCell(1);
-    // let textNode1 = document.createTextNode(item.event_date);
-    // td3.appendChild(textNode1);
-
-    // let td4 = tr.insertCell(2);
-    // let textNode2 = document.createTextNode(item.theme);
-    // td4.appendChild(textNode2);
-
-    // let td5 = tr.insertCell(3);
-    // td5.appendChild(editButton);
-
-    // let td6 = tr.insertCell(4);
-    // td6.appendChild(deleteButton);
-  });
-
-  todos = data;
-}
