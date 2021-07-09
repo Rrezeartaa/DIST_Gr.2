d002ad01bd8e1e9cf74849e92d7b31005cf02116
vanesarama <56617704+vanesarama@users.noreply.github.com>
2021-07-09 09:46:06 +0200

    Update fileController.js

--- controllers/fileController.js
+++ controllers/fileController.js
@@ -10,3 +10,9 @@ const upload = async (req, res) => {
     // if (req.file == undefined) {
     //   return res.status(400).send({ message: "Please upload a file!" });
     // }
+
+     res.status(200).send({
+      message: "Uploaded the file successfully: " + req.file.originalname,
+    });
+  } catch (err) {
+    console.log(err);
