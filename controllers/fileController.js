83cf593d6e11c3f674d0f9cb989ba3f7b3844abc
vanesarama <56617704+vanesarama@users.noreply.github.com>
2021-07-09 09:45:48 +0200

    Update fileController.js

--- controllers/fileController.js
+++ controllers/fileController.js
@@ -1,3 +1,12 @@
 const uploadFile = require("../upload");
 const fs = require("fs");
 const baseUrl = "http://localhost:3000/files/";
+
+
+const upload = async (req, res) => {
+  try {
+    await uploadFile(req, res);
+
+    // if (req.file == undefined) {
+    //   return res.status(400).send({ message: "Please upload a file!" });
+    // }
