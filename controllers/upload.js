d0e3c1bceeb97a51a3ce50b98e5a3aa477a39eba
vanesarama <56617704+vanesarama@users.noreply.github.com>
2021-07-09 11:12:19 +0200

    Update upload.js

--- controllers/upload.js
+++ controllers/upload.js
@@ -4,6 +4,9 @@ const maxSize = 2 * 1024 * 1024;
 
 
 let storage = multer.diskStorage({
+  
   destination: (req, file, cb) => {
     cb(null, "../DIST_PROJECT_Gr.2/uploads/");
   },
+  filename: (req, file, cb) => {
+    console.log(file.originalname);
